fdb0dcb7f9e248dfc43f9e077cb52e772c1d8a20
prabhakar <prabhakarkumar16181@gmail.com>
2024-03-15 08:58:37 -0700

    Created Basic UI

---
+++ src/component/Main.jsx
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+      <div className="component">
+        <span>Main</span>
+      </div>
+    )
+  }
+  
+  export default Main
---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,9 @@
+function Sidebar() {
+    return (
+      <div className="component">
+        <span>Sidebar</span>
+      </div>
+    )
+  }
+  
+  export default Sidebar
